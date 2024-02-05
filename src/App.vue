8cdbdcd8081b81a0e97c871f34444a37cc03dc54
fajartc02 <fajartc02@gmail.com>
2024-02-05 23:27:39 +0700

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
 <template>
+  <button class="btn-secondary position-absolute top-0 end-0" type="button" data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+      height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+      stroke-linejoin="round" class="feather feather-menu">
+      <line x1="3" y1="12" x2="21" y2="12"></line>
+      <line x1="3" y1="6" x2="21" y2="6"></line>
+      <line x1="3" y1="18" x2="21" y2="18"></line>
+    </svg></button>
+
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+      <h5 id="offcanvasRightLabel">DPT OEE Monitoring</h5>
+      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div class="list-group">
+        <a class="list-group-item list-group-item-action active">Dashboard</a>
+        <a class="list-group-item list-group-item-action">OEE History</a>
+        <a class="list-group-item list-group-item-action">Parameter History</a>
+      </div>
+    </div>
+  </div>
   <router-view />
 </template>
 
